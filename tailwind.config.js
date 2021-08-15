726911f2a0ba9db7f5fc91f700ddef305fe60a52
Aman Gupta <amankumargupta@analytic-edge.com>
2021-08-15 16:14:56 +0530

    add global theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ module.exports = {
   important: true,
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        "button-primary": "var(--button-primary)",
+        input: "var(--input)",
+        text: "var(--text)",
+        "text-primary": "var(--text-primary)",
+        placeholder: "var(--placeholder)",
+        "form-label": "var(--form-label)",
+        "input-border": "var(--form-border)",
+        "text-crumb-active": "var(--text-crumb-active)",
+        crumb: "var(--crumb)",
+        "crumb-active": "var(--crumb-active)",
+        "crumb-completed": "var(--crumb-completed)",
+      },
+      fontFamily: {
+        sofia: ["Sofia Pro", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
